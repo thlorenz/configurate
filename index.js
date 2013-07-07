bf8fa65bf33c77d46283e158d412cf58e69b58d6
Thorsten Lorenz <thlorenz@gmx.de>
2013-07-07 12:38:53 -0400

    workflow: missing config, default given, edits applied and saved

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
 
-var path =  require('path')
-  , fs   =  require('fs')
-  , HOME =  process.env.HOME
+var path   =  require('path')
+  , fs     =  require('fs')
+  , mkdirp =  require('mkdirp')
+  , runnel =  require('runnel')
+  , HOME   =  path.join(__dirname, 'test', 'fixtures', 'home') // process.env.HOME
   ;
 
 function copy(srcFile, tgtFile, cb) {
@@ -19,6 +21,20 @@ function copy(srcFile, tgtFile, cb) {
   readStream.pipe(writeStream);
 }
 
+function copyDefaultConfig(defaultConfig, configFile, cb) {
+  copy(defaultConfig, configFile, function (err) {
+    cb(err, configFile);  
+  });
+}
+
+function mkconfigDir (configFile, cb) {
+  // does nothing if dir already exists
+  var dir = path.dirname(configFile);
+  mkdirp(dir, function (err) {
+    cb(err, configFile);
+  });
+}
+
 function resolvePath (p) {
   if (!p) return HOME;
 
@@ -29,36 +45,124 @@ function resolvePath (p) {
 }
 
 function defaultLoad (p, cb) {
-  setTimeout(function () {
+  process.nextTick(function () {
     try {
       cb(null, require(p));
     } catch (err) {
       cb(err);
     }
-  }, 0);
+  });
 }
+
+function defaultSerialize (config, cb) {
+  process.nextTick(function () {
+    try {
+      var s = 'module.exports = ' + JSON.stringify(config)
+      cb(null, s);
+    } catch (err) {
+      cb(err);
+    }
+  });
+}
+
+
+function noConfig (configFile, cb) {
+  process.nextTick(cb.bind(null, null, null));
+}
+
+
+/**
+ * 
+ * Possible workflows:
+ *
+ *  config exists:
+ *    - mkdir
+ *    - load config
+ *    - edit loaded config
+ *    - serialize edited config
+ *    - write config
+ *
+ *  config doesn't exist and default config is given:
+ *    - mkdir
+ *    - copy default config
+ *    - load copied config
+ *    - edit loaded config
+ *    - serialize edited config
+ *    - write config
+ *
+ *  config doesn't exist and no default config is given:
+ *    - mkdir
+ *    - edit config starting from scratch (null) 
+ *    - serialize edited config
+ *    - write config
+ *    
+ * @name 
+ * @function
+ * @return void
+ */
 
 var go = module.exports = function (opts, cb) {
   opts = opts || {};
 
   var defaultConfig =  opts.defaultConfig || null
     , configDir     =  resolvePath(opts.configDir)
-    , configFile    =  path.join(configDir, opts.configFile);
+    , configFile    =  path.join(configDir, opts.configFile)
+    , serialize     =  opts.serialize || defaultSerialize
+    , edit          =  opts.edit
+
 
   var loadConfig = opts.loadConfig || defaultLoad;
   
+  var tasks = [ runnel.seed(configFile), mkconfigDir ];
+
   fs.exists(configFile, function (exists) {
-    if (exists) return loadConfig(configFile, cb);
-    if (defaultConfig) console.error('TODO: copy default config and load it');
+    if (!exists) { 
+      if (defaultConfig) {
+        tasks.push(copyDefaultConfig.bind(null, defaultConfig));
+        tasks.push(loadConfig);
+      } else {
+        tasks.push(noConfig);
+      }
+    } else {
+      tasks.push(loadConfig);
+    }
+
+    tasks = tasks.concat(
+      [ edit
+      , serialize
+      , fs.writeFile.bind(fs, configFile)
+      , cb
+      ]
+    )
+
+    runnel(tasks);
   });
 };
 
-var config = function (conf, cb) {
-  console.log('filling in', conf);
-
+var edit = function (config, cb) {
+  console.log('filling in', config);
+  process.nextTick(function () {
+    config.added = 'hello';
+    cb(null, config);
+  });
 }
 
-go({ configDir: '.config', configFile: 'testlingify.js' }, function (err, conf) {
-  if (err) return console.error(err);
-  console.log(conf);
+// -- emit events
+
+var defaultConfig = path.join(__dirname, 'test', 'fixtures', 'defaults', 'cjs-empty.js');
+
+try { fs.unlinkSync(path.join(HOME, '.config')); } catch (_) {}
+
+go(
+    { configDir     :  '.config'
+    , configFile    :  'testlingify.js'
+    , defaultConfig :  defaultConfig
+    , edit          :  edit
+    }
+  , function (err, conf) {
+    if (err) return console.error(err);
+    console.log(conf);
 });
+
+
+
