7fca382404479d22f2fdfc294f6bd289e2c58c48
Thorsten Lorenz <thlorenz@gmx.de>
2013-07-08 22:15:59 -0400

    using sinless to not have to worry about async/sync versions of user provided functions

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
 
-var path   =  require('path')
-  , fs     =  require('fs')
-  , mkdirp =  require('mkdirp')
-  , runnel =  require('runnel')
-  , HOME   =  path.join(__dirname, 'test', 'fixtures', 'home') // process.env.HOME
+var path    =  require('path')
+  , fs      =  require('fs')
+  , mkdirp  =  require('mkdirp')
+  , runnel  =  require('runnel')
+  , sinless =  require('sinless')
+  , HOME    =  path.join(__dirname, 'test', 'fixtures', 'home') // process.env.HOME
   ;
 
 function copy(srcFile, tgtFile, cb) {
@@ -44,32 +45,17 @@ function resolvePath (p) {
   return isFullPath ? p : path.join(HOME, p);
 }
 
-function defaultLoad (p, cb) {
-  process.nextTick(function () {
-    try {
-      cb(null, require(p));
-    } catch (err) {
-      cb(err);
-    }
-  });
+function defaultLoad (p) {
+  return require(p);
 }
 
-function defaultSerialize (config, cb) {
-  process.nextTick(function () {
-    try {
-      var s = 'module.exports = ' + JSON.stringify(config)
-      cb(null, s);
-    } catch (err) {
-      cb(err);
-    }
-  });
+function defaultSerialize (config) {
+  return 'module.exports = ' + JSON.stringify(config)
 }
-
 
 function noConfig (configFile, cb) {
-  process.nextTick(cb.bind(null, null, null));
+  setImmediate(cb.bind(null, null, null));
 }
-
 
 /**
  * 
@@ -107,11 +93,12 @@ var go = module.exports = function (opts, cb) {
   var defaultConfig =  opts.defaultConfig || null
     , configDir     =  resolvePath(opts.configDir)
     , configFile    =  path.join(configDir, opts.configFile)
-    , serialize     =  opts.serialize || defaultSerialize
-    , edit          =  opts.edit
+    , serialize     =  sinless(opts.serialize || defaultSerialize)
+    , edit          =  sinless(opts.edit)
+    ;
 
 
-  var loadConfig = opts.loadConfig || defaultLoad;
+  var loadConfig = sinless(opts.loadConfig || defaultLoad);
   
   var tasks = [ runnel.seed(configFile), mkconfigDir ];
 
@@ -139,19 +126,17 @@ var go = module.exports = function (opts, cb) {
   });
 };
 
-var edit = function (config, cb) {
-  console.log('filling in', config);
-  process.nextTick(function () {
-    config.added = 'hello';
-    cb(null, config);
-  });
+var edit = function (config) {
+  config.added = 'hiiiiello';
+  return config;
 }
 
 // -- emit events
 
 var defaultConfig = path.join(__dirname, 'test', 'fixtures', 'defaults', 'cjs-empty.js');
+var configPath = path.join(HOME, '.config');
 
-try { fs.unlinkSync(path.join(HOME, '.config')); } catch (_) {}
+try { fs.unlinkSync(configPath); } catch (_) {}
 
 go(
     { configDir     :  '.config'
@@ -161,7 +146,7 @@ go(
     }
   , function (err, conf) {
     if (err) return console.error(err);
-    console.log(conf);
+    console.log(require(configPath + '/testlingify.js'));
 });
 
 
